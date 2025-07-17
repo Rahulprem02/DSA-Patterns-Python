d0803bebb56b9963dddba060dc06fcb9bf93cc0f
Rahul <rahulprem02@gmail.com>
2025-07-16 23:41:48 -0700

    724. Find Pivot Index

---
+++ PrefixTracking/find_pivot_index.py
@@ -0,0 +1,21 @@
+
+# Leetcode 724: https://leetcode.com/problems/find-pivot-index/
+# Pattern: Prefix Sum 
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        leftSum = 0
+        totalSum = sum(nums)
+
+        for i in range(len(nums)):
+            
+            rightSum = totalSum - leftSum - nums[i]
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+        return -1
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)

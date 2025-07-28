d2469d5d663450362d395825ef35a130a9c9097e
Rahul <rahulprem02@gmail.com>
2025-07-28 11:57:07 +0500

    27. Remove Element

---
+++ TwoPointers/remove_element.py
@@ -0,0 +1,38 @@
+
+
+
+# Leetcode 27: https://leetcode.com/problems/remove-element
+# Pattern: Two Pointer Appproch
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+    
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+
+
+#class Solution:
+ #   def removeElement(self, nums: List[int], val: int) -> int:
+        # result = []
+        # for i in range(len(nums)):
+        #     if nums[i]!=val:
+        #         res.append(nums[i])
+            
+        # nums[:len(result)] = res
+        # return len(result)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+
+       
+
+                 
+        

1c06ebd5db56907c4656bdf90a77b5ff8457b818
Rahul <rahulprem02@gmail.com>
2025-08-09 22:49:56 +0500

    977. Squares of a Sorted Array

---
+++ TwoPointers/squares_of_a_sorted_array.py
@@ -0,0 +1,18 @@
+
+# Leetcode 977: https://leetcode.com/problems/squares-of-a-sorted-array/
+# Pattern: Two Pointer Appproch
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        squared = []
+        for num in nums:
+            squared.append(num * num)
+        
+        squared.sort()
+        
+        return squared
+
+
+    # Time Complexity: O(nlogn)
+    # Space Complexity: O(n)
+        
